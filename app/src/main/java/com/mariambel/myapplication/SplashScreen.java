c6495e153a5e28ba421b9f9d556f321891a6cc8c
mariam-bel <merimarabel@gmail.com>
2025-10-09 12:14:13 +0200

    Algunos cambios en el fondo y transiciones

--- app/src/main/java/com/mariambel/myapplication/SplashScreen.java
+++ app/src/main/java/com/mariambel/myapplication/SplashScreen.java
@@ -5,18 +5,44 @@ import android.graphics.Insets;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+
 public class SplashScreen extends AppCompatActivity {
+
+    ImageView fondo;
+    ImageView splashLogo;
+    TextView nombreApp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
+
+        Animation fade = AnimationUtils.loadAnimation(this,R.anim.fading);
+
+        nombreApp = findViewById(R.id.splashAppName);
+        nombreApp.startAnimation(fade);
+        splashLogo = findViewById(R.id.splashLogo);
+        splashLogo.startAnimation(fade);
+
+        fondo = findViewById(R.id.splashImagenFondo);
+        Glide.with(this)
+                .load("https://images.unsplash.com/photo-1495147466023-ac5c588e2e94?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+                .transition(DrawableTransitionOptions.withCrossFade(1000))
+                .centerCrop()
+                .into(fondo);
 
         launchNextActivity();
     }

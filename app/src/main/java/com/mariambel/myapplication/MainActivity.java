da3f5512ec9185e26f233aaac9048bd6b9d23f60
mariam-bel <merimarabel@gmail.com>
2025-11-21 13:51:56 +0100

    tarjetas creadas

--- app/src/main/java/com/mariambel/myapplication/MainActivity.java
+++ app/src/main/java/com/mariambel/myapplication/MainActivity.java
@@ -10,10 +10,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager.widget.ViewPager;
+
+import com.mariambel.myapplication.databinding.ActivityMainBinding;
+import com.mariambel.myapplication.frmanager.Paginador;
 
 public class MainActivity extends AppCompatActivity {
     int contador;
     Button mainButton;
+
+    private ActivityMainBinding binding;
     TextView mainTV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,24 +33,21 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Paginador paginador = new Paginador(this,getSupportFragmentManager());
+        ViewPager viewPager = binding.viewPager;
+        viewPager.setAdapter(paginador);
+
+
+
         Bundle bundle = getIntent().getExtras();
         if (bundle != null) {
             String tiempo = String.valueOf(bundle.getLong("tiempoInvertido", 0));
             mainTV.setText(tiempo+" ha tardado en llenar el login.");
         }
 
-        mainButton = findViewById(R.id.mainButton);
-        mainTV = findViewById(R.id.mainTV);
 
-        contador = 0;
-        mainTV.setText(String.valueOf(contador));
-
-        mainButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                contador++;
-                mainTV.setText(String.valueOf(contador));
-            }
-        });
     }
 }
